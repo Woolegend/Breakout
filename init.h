a8b7c25da5f32b0bacbfcfc62e668e81ff8b0a98
Woolegend <dnwogus4260@naver.com>
2023-06-10 21:47:35 +0900

    add brick defines

--- init.h
+++ init.h
@@ -6,12 +6,33 @@
 
 #define        NUMOFWALL            10
 
+
+#define        SCALE                30.0
+
+
 #define        PI                   3.1415
 #define        BALLSLICE            20
 #define        RADIUS               10
 
-#define        BRICK_COL            20
-#define        BRICK_ROW            16
 
-#define        SCALE                30.0
 
+#define			BRICK_COL				20
+#define			BRICK_ROW				16
+
+#define			BRICK_LEFT_TOP			0
+#define			BRICK_TOP				1
+#define			BRICK_RIGHT_TOP			2
+#define			BRICK_RIGHT				3
+#define			BRICK_RIGHT_BOTTOM		4
+#define			BRICK_BOTTOM			5
+#define			BRICK_LFET_BOTTOM		6
+#define			BRICK_LEFT				7
+
+#define			BRICK_NORMAL			0
+#define			BRICK_ITEMBOX			1
+#define			BRICK_ITEM				2
+ 
+#define			ITEM_NONE				0
+#define			ITEM_MUSH				1
+#define			ITEM_FLOWER				2
+#define			ITEM_STAR				3
